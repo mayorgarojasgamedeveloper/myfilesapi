55afa293112347ac4fefb87f6c8a58e50aebc8a6
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-03 16:11:46 -0500

    Se agrego el proyecto y el archivo con las direcciones de la api.

---
+++ db/index.js
@@ -0,0 +1,70 @@
+var {Client} = require('pg');
+var Router = require('express-promise-router');
+
+var URL = 'postgres://postgres:admin@localhost/academic';
+
+var client = new Client(URL);
+
+client.connect((err) => {
+  if(err)
+    console.error(err);
+  else
+    console.log('Database Started');
+});
+
+exports.INSERT = async function(table_name, columns, values, contition = null, returning = null) {
+
+  var sql = ``;
+  sql += `INSERT INTO ${table_name} (${columns})`;
+  sql += ` VALUES (${values})`;
+  if(contition !== null)
+    sql += ` WHERE ${contition}`;
+  if(returning !== null)
+    sql += ` RETURNING ${returning}`;
+
+  const { rows } = await client.query(sql);
+  return rows;
+}
+
+exports.SELECT = async function(table_name, columns = '*', condition = null) {
+
+  var sql = ``;
+  sql += `SELECT ${columns}`;
+  sql += ` FROM ${table_name}`;
+  if(condition !== null)
+    sql += ` WHERE ${condition}`;
+
+  const { rows } = await client.query(sql);
+  console.log(rows);
+  return rows;
+}
+
+exports.UPDATE = async function(table_name, setters, condition, returning = null) {
+
+  var sql = ``;
+  sql += `UPDATE ${table_name}`;
+  sql += ` SET ${setters}`;
+  sql += ` WHERE ${condition}`;
+  if(returning !== null)
+    sql += ` RETURNING ${returning}`;
+
+  const { rows } = await client.query(sql);
+  return rows;
+}
+
+exports.DELETE = async function(table_name, condition) {
+
+  var sql = ``;
+  sql += `DELETE FROM ${table_name}`;
+  sql += ` WHERE ${condition}`;
+  const { rows } = await client.query(sql);
+  return rows;
+}
+
+exports.NOW = async function() {
+
+  var sql = `SELECT NOW()`;
+  const { rows } = await client.query(sql);
+  console.log(rows);
+  return rows;
+}
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var db = require('../db');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  var response = await db.NOW();
+  res.send(`Bienvendio a la API de Academic.Archives [${response[0].now}]`);
+});
+
+module.exports = router;
